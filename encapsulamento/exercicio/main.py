831c7d0b3bddc6b645750b7f9438af482c19848d
Desconhecido <lucas_matias@protonmail.com>
2021-03-04 17:21:01 -0300

    primeiro commit

---
+++ encapsulamento/exercicio/main.py
@@ -0,0 +1,21 @@
+"""
+    Módulo principal
+"""
+
+
+
+
+
+
+
+
+
+def main():
+    """
+        Função principal
+        Responsável por imprimir saida
+    """
+
+
+if __name__ == '__main__':
+    main()
