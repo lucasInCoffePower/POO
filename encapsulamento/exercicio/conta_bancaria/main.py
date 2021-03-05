9cfa93a7b9224a87e67167b01b96d65e59f5ad80
Desconhecido <lucas_matias@protonmail.com>
2021-03-05 02:30:07 -0300

    Concluindo projeto banco

--- encapsulamento/exercicio/conta_bancaria/main.py
+++ encapsulamento/exercicio/conta_bancaria/main.py
@@ -12,6 +12,13 @@ def main():
         Responsável por imprimir na tela
     """
 
+    c1 = ContaBancaria("Daniel Silva", 45, "15151561515") # Criando objeto
+    print(c1) # mostrando objeto
+    print(c1.saldo) # verificando o valor no atributo saldo
+    c1.deposito(500) # Adicionanod saldo a conta
+    print(c1.saldo) # Verificando o taldo tm
+    c1.titular = "João"
+    print(c1)
 
 if __name__ == '__main__':
     main()
